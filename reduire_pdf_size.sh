c7367bbf1ce0a6c7e1ee690cd44a53c95109d8e3
RRTracer <rrtracer@gmail.com>
2024-10-27 21:31:42 +0100

    ajout dossier deauth etc ... ainsi que du code d'auto_deauth.sh ainsi que de la procédure de bruteforce wifi ainsi que pdf reduce size donner par monsieur Bachard

---
+++ reduire_pdf_size.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+entree=$1
+
+sortie=${1/.pdf/_reduit.pdf}
+
+#type=/screen
+type=/ebook
+
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=${type} -dNOPAUSE -dQUIET -dBATCH -sOutputFile=${sortie} ${entree}
