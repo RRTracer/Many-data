b249d6de87c48f5f600cca2667efea886830dc91
RRTracer <rrtracer@gmail.com>
2024-11-12 16:43:50 +0100

    modification du sript et résolution du problème de chiffrement des mots de passe entrer via read par la commande grub-mkpassword-pbkdf2, et ajout d'une ligne en pied de code avant le return 0 qui est le unset afin de vider le cache des variables

--- script_secure_grub/secure_grub.sh
+++ script_secure_grub/secure_grub.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # |-------------------------------------------------------------------------------------------|
-# |        Ce script à été réaliser dans le but de sécurisé les ordinateurs utilisant grub en |
-# | les os de la même manières que avant. Ce script a été fait par RRTracer . Tous droit      |
-# | réserver.                                                                                 |
-# |  Ce code est sous license : Licence Creative-Commons by-sa                                |
-# |  Lien de la license :                                                                     |
+# |        Ce script a été réalisé dans le but de sécuriser les ordinateurs utilisant GRUB    |
+# | pour protéger les OS tout en permettant un démarrage normal. Ce script a été écrit par    |
+# | RRTracer. Tous droits réservés.                                                           |
+# |  Ce code est sous licence : Licence Creative-Commons by-sa                                |
+# |  Lien de la licence :                                                                     |
 # |         https://creativecommons.org/licenses/by-sa/2.0/fr/                                |
 # | Cela signifie que vous pouvez télécharger, utiliser tout ou partie, modifier, partager ce |
 # | code à votre guise.                                                                       |
 # |                                                                                           |
-# |      En contrepartie, cela implique que si vous les utilisez, vous devrez :               |
+# |      En contrepartie, cela implique que si vous l'utilisez, vous devrez :                 |
 # |                                                                                           |
-# |              les partager sous une licence au moins équivalente (libre)                   |
+# |              le partager sous une licence au moins équivalente (libre)                    |
 # |                                                                                           |
-# |             marquer le nom de l'auteur de manière explicite.                              |
+# |             mentionner le nom de l'auteur de manière explicite.                           |
 # |                                                                                           |
 # |-------------------------------------------------------------------------------------------|
 
@@ -23,12 +23,39 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-echo "Entrez un user pour le grub : "
-read -s grub_user
+# Vérifier si expect est installé
+if ! command -v expect &> /dev/null; then
+    echo "Le paquet 'expect' est requis mais n'est pas installé."
+    read -p "Voulez-vous l'installer maintenant ? (o/n) " choice
+    if [[ "$choice" =~ ^[Oo]$ ]]; then
+        apt update && apt install -y expect
+        if [[ $? -ne 0 ]]; then
+            echo "L'installation de 'expect' a échoué. Veuillez l'installer manuellement."
+            exit 1
+        fi
+    else
+        echo "Expect est nécessaire pour exécuter ce script. Veuillez l'installer et réessayez."
+        exit 1
+    fi
+fi
+
+echo "Entrez un utilisateur pour le GRUB : "
+read grub_user
+
 echo "Entrez le mot de passe pour GRUB :"
 read -s grub_password
 
-grub_password_hash=$(grub-mkpasswd-pbkdf2 <<< "$grub_password" | grep 'grub.pbkdf2' | awk '{print $NF}')
+# Utiliser "expect" pour générer le hash de mot de passe pour grub
+grub_password_hash=$(expect <<EOF
+spawn grub-mkpasswd-pbkdf2
+expect "Enter password: "
+send "$grub_password\r"
+expect "Reenter password: "
+send "$grub_password\r"
+expect eof
+EOF
+)
+grub_password_hash=$(echo "$grub_password_hash" | grep 'grub.pbkdf2' | awk '{print $NF}')
 
 echo "Ajout du mot de passe chiffré dans /etc/grub.d/40_custom..."
 
@@ -43,23 +70,16 @@ update-grub
 
 echo "Modification du fichier /boot/grub/grub.cfg pour ajouter l'option --unrestricted..."
 
-debian_added=false
-windows_added=false
+# Modifier les lignes de menu Debian et Windows pour ajouter --unrestricted après "os"
+sed -i '/menuentry.*Debian/ s/\(os\)/\1 --unrestricted/' /boot/grub/grub.cfg
+sed -i '/menuentry.*Windows/ s/\(os\)/\1 --unrestricted/' /boot/grub/grub.cfg
 
-while IFS= read -r line; do
-    if [[ $line =~ "menuentry.*Debian" && $debian_added == false ]]; then
-        sed -i "/menuentry.*Debian/ s/$/ --unrestricted/" /boot/grub/grub.cfg
-        debian_added=true
-    fi
-    
-    if [[ $line =~ "menuentry.*Windows" && $windows_added == false ]]; then
-        sed -i "/menuentry.*Windows/ s/$/ --unrestricted/" /boot/grub/grub.cfg
-        windows_added=true
-    fi
-done < /boot/grub/grub.cfg
 
-echo "GRUB a été configuré avec succès et protégé avec un mot de passe."
+echo "GRUB a été configuré avec succès et protégé par un mot de passe."
 echo "Si vous avez oublié le mot de passe, aucun contournement n'est possible."
 echo "Redémarrez votre machine pour que les changements prennent effet."
+
+# Nettoyage des variables afin d'éviter le stockage en cahce
+unset grub_user grub_password grub_password_hash
 
 exit 0
